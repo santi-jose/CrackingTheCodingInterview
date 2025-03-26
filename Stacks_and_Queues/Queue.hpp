b1f6a3cb3fc02d8e5c9ca007392507eea079db35
santi-jose <jlsantia21@gmail.com>
2025-03-25 22:53:55 -0700

    Added Queue and Stack data structure header files for implementation. Defined all functions that describe the Queue and Stack data structure from the Cracking the Coding Interview textbook.

---
+++ Stacks_and_Queues/Queue.hpp
@@ -0,0 +1,17 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+#include "../LinkedList/LinkedList.hpp" // Reference LinkedList header
+
+class Queue{
+    private:
+        LinkedList* list; // list to store for queue
+
+    public:
+        void add(string data); // Add an item to the end of the list
+        string remove(); // remove the first item in the list
+        string peek(); // peek at the value at the front of the list
+        bool isEmpty(); // return true if an d only i the queue is empty
+};
+
+#endif
---
+++ Stacks_and_Queues/Stack.hpp
@@ -0,0 +1,17 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "../LinkedList/LinkedList.hpp" // Reference LinkedList header
+
+class Stack{
+    private:
+        LinkedList* list; // list to store for stack
+
+    public:
+        string pop(); // remove the top item from the stack
+        void push(string data); // add an item to the top of the stack
+        string peek(); // return the data at the top of the stack
+        bool isEmpty(); // return true if and only if the stack is empty
+};
+
+#endif
