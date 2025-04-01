72609a4c7fd2bc50595de3c393ea11b95ad7d4e0
santi-jose <jlsantia21@gmail.com>
2025-03-31 21:43:31 -0700

    Added delete Queue at bottom of main.cpp.

--- Stacks_and_Queues/main.cpp
+++ Stacks_and_Queues/main.cpp
@@ -5,8 +5,8 @@
 
 int main(){
     // test Stack and Queue data structures
-    Stack S1;
-    Queue Q1;
+    Stack* S1 = new Stack();
+    Queue* Q1 = new Queue();
 
     // create abc string
     string abc_string[] = {"a", "b", "c", "d", "e", "f", "g",
@@ -15,26 +15,26 @@ int main(){
         "v", "w", "x", "y", "z"};
 
     // test empty function
-    if (S1.isEmpty())
+    if (S1->isEmpty())
     {
-        cout << "isEmpty() test passed. Stack S1.isEmpty(): " << 
-        (S1.isEmpty() == true ? "true" : "false") << endl;
+        cout << "isEmpty() test passed-> Stack S1->isEmpty(): " << 
+        (S1->isEmpty() == true ? "true" : "false") << endl;
     }
     else
     {
-        cout << "isEmpty() test failed. Stack S1.isEmpty(): " <<
-        ( S1.isEmpty() == true ? "true" : "false" ) << endl;
+        cout << "isEmpty() test failed-> Stack S1->isEmpty(): " <<
+        ( S1->isEmpty() == true ? "true" : "false" ) << endl;
     }
 
-    if (Q1.isEmpty())
+    if (Q1->isEmpty())
     {
-        cout << "isEmpty() test passed. Queue Q1.isEmpty(): " << 
-        (Q1.isEmpty() == true ? "true" : "false") << endl;
+        cout << "isEmpty() test passed-> Queue Q1->isEmpty(): " << 
+        (Q1->isEmpty() == true ? "true" : "false") << endl;
     }
     else
     {
-        cout << "isEmpty() test failed. Queue Q1.isEmpty(): " << 
-        (Q1.isEmpty() == true ? "true" : "false") << endl;
+        cout << "isEmpty() test failed-> Queue Q1->isEmpty(): " << 
+        (Q1->isEmpty() == true ? "true" : "false") << endl;
     }
 
     // create two test linked lists
@@ -42,78 +42,81 @@ int main(){
     {
         // test insertHead function
         cout << "Q1: " << endl;
-        Q1.add(it);        // insert node at the head of linked list
-        Q1.printQueue(); // print current linked list
+        Q1->add(it);        // insert node at the head of linked list
+        Q1->printQueue(); // print current linked list
 
         // test insertTail function
         cout << "S1: " << endl;
-        S1.push(it); // insert node at tail of linked list
-        S1.printStack();
+        S1->push(it); // insert node at tail of linked list
+        S1->printStack();
     }
 
     // test empty function
-    if (S1.isEmpty())
+    if (S1->isEmpty())
     {
-        cout << "isEmpty() test failed. Stack S1.isEmpty(): " << 
-        (S1.isEmpty() == true ? "true" : "false") << endl;
+        cout << "isEmpty() test failed-> Stack S1->isEmpty(): " << 
+        (S1->isEmpty() == true ? "true" : "false") << endl;
     }
     else
     {
-        cout << "isEmpty() test passed. Stack S1.isEmpty(): " <<
-        ( S1.isEmpty() == true ? "true" : "false" ) << endl;
+        cout << "isEmpty() test passed-> Stack S1->isEmpty(): " <<
+        ( S1->isEmpty() == true ? "true" : "false" ) << endl;
     }
 
-    if (Q1.isEmpty())
+    if (Q1->isEmpty())
     {
-        cout << "isEmpty() test failed. Queue Q1.isEmpty(): " << 
-        (Q1.isEmpty() == true ? "true" : "false") << endl;
+        cout << "isEmpty() test failed-> Queue Q1->isEmpty(): " << 
+        (Q1->isEmpty() == true ? "true" : "false") << endl;
     }
     else
     {
-        cout << "isEmpty() test passed. Queue Q1.isEmpty(): " << 
-        (Q1.isEmpty() == true ? "true" : "false") << endl;
+        cout << "isEmpty() test passed-> Queue Q1->isEmpty(): " << 
+        (Q1->isEmpty() == true ? "true" : "false") << endl;
     }
 
     // test removal functions
 
     // while stack is not empty
-    while(!S1.isEmpty()){
-        string cData = S1.pop();
-        cout << "Removed " << cData << ", from S1 Stack." << endl;
+    while(!S1->isEmpty()){
+        string cData = S1->pop();
+        cout << "Removed " << cData << ", from S1 Stack->" << endl;
         cout << "S1: " << endl;
-        S1.printStack();
+        S1->printStack();
     }
 
     // while quque is not empty
-    while(!Q1.isEmpty()){
-        string cData = Q1.remove(); 
-        cout << "Removed " << cData << ", from Q1 Queue." << endl;
+    while(!Q1->isEmpty()){
+        string cData = Q1->remove(); 
+        cout << "Removed " << cData << ", from Q1 Queue->" << endl;
         cout << "Q1: " << endl;
-        Q1.printQueue();
+        Q1->printQueue();
     }
 
     // test isEmpty function
-    if (S1.isEmpty())
+    if (S1->isEmpty())
     {
-        cout << "isEmpty() test passed. Stack S1.isEmpty(): " << 
-        (S1.isEmpty() == true ? "true" : "false") << endl;
+        cout << "isEmpty() test passed-> Stack S1->isEmpty(): " << 
+        (S1->isEmpty() == true ? "true" : "false") << endl;
     }
     else
     {
-        cout << "isEmpty() test failed. Stack S1.isEmpty(): " <<
-        ( S1.isEmpty() == true ? "true" : "false" ) << endl;
+        cout << "isEmpty() test failed-> Stack S1->isEmpty(): " <<
+        ( S1->isEmpty() == true ? "true" : "false" ) << endl;
     }
 
-    if (Q1.isEmpty())
+    if (Q1->isEmpty())
     {
-        cout << "isEmpty() test passed. Queue Q1.isEmpty(): " << 
-        (Q1.isEmpty() == true ? "true" : "false") << endl;
+        cout << "isEmpty() test passed-> Queue Q1->isEmpty(): " << 
+        (Q1->isEmpty() == true ? "true" : "false") << endl;
     }
     else
     {
-        cout << "isEmpty() test failed. Queue Q1.isEmpty(): " << 
-        (Q1.isEmpty() == true ? "true" : "false") << endl;
+        cout << "isEmpty() test failed-> Queue Q1->isEmpty(): " << 
+        (Q1->isEmpty() == true ? "true" : "false") << endl;
     }
+
+    delete S1;
+    delete Q1;
 
     // return 0 on successful execution
     return 0;
